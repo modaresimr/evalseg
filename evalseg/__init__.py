7108e1671336eaa3b6c34f02f3faba74e47c49a5
Ali <modaresimr@gmail.com>
2022-10-22 20:37:44 +0200

    update

--- evalseg/__init__.py
+++ evalseg/__init__.py
@@ -3,21 +3,10 @@ import importlib
 import sys
 from os.path import basename, dirname, isfile
 
-from . import common, geometry, io, metrics, ui, ui3d
-
-modules = glob.glob(dirname(__file__) + "/*.py")
-__all__ = [
-    basename(f)[:-3]
-    for f in modules
-    if isfile(f) and not basename(f).startswith("__")
-]  # exclude __init__.py
-from . import *
-
-# print(__all__)
+from . import common, compress, ct, geometry, io, metrics, ui, ui3d
 
 
 def reload():
     for module in list(sys.modules.values()):
-        if "eval_seg." in f"{module}":
-            # print(module)
+        if "evalseg." in f"{module}":
             importlib.reload(module)
