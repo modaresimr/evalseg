9e9cc9d982fd82c3e786ec76aa19364f1019becc
Ali <modaresimr@gmail.com>
2022-11-24 23:08:44 +0100

    add assert for dict op same keys

--- evalseg/common/dict_op.py
+++ evalseg/common/dict_op.py
@@ -31,3 +31,23 @@ def have_same_keys(dic1, dic2):
                 return False
 
     return True
+
+
+def assert_same_keys(dic1, dic2):
+    if type(dic1) != dict and type(dic2) == dict:
+        assert False, f'type {dic1} is different from type {dic2}'
+    if type(dic1) == dict and type(dic2) != dict:
+        assert False, f'type {dic1} is different from type {dic2}'
+
+    if type(dic1) == dict and type(dic2) == dict:
+        if not all(k in dic2 for k in dic1):
+            x = [k for k in dic1 if k not in dic2]
+            assert False, f' {x} is in dic1={dic1}  but not in dic2 {dic2}'
+        if not all(k in dic1 for k in dic2):
+            x = [k for k in dic2 if k not in dic1]
+            assert False, f' {x} is in dic2={dic2}  but not in dic1 {dic1}'
+
+        for k in dic2:
+            assert_same_keys(dic1[k], dic2[k])
+
+    return True
--- evalseg/metrics/average.py
+++ evalseg/metrics/average.py
@@ -12,7 +12,7 @@ class Average:
     def add(self, metric_out):
         if len(self.__items) == 0:
             self.__items = dict_op.multiply(metric_out, 0)
-        assert dict_op.have_same_keys(metric_out, self.__items)
+        dict_op.assert_same_keys(metric_out, self.__items)
         self.__items = dict_op.sum(self.__items, metric_out)
         self.__count += 1
 
@@ -32,7 +32,7 @@ class MultiSystemAverage:
         if len(self.__items) == 0:
             self.__items = {p: Average() for p in pred_metric_out}
 
-        assert dict_op.have_same_keys({p: 0 for p in pred_metric_out}, self.__items)
+        dict_op.assert_same_keys({p: 0 for p in pred_metric_out}, self.__items)
 
         for p in self.__items:
             self.__items.add(pred_metric_out[p])
