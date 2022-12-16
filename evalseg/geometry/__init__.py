a06c9c59a7a1e6fd9f02ab86e6a463c84d4deb5e
modaresimr <modaresi.mr@gmail.com>
2022-12-16 18:57:42 +0000

    add calc_max_slice_idx

--- evalseg/geometry/__init__.py
+++ evalseg/geometry/__init__.py
@@ -3,5 +3,5 @@ from .distance import distance
 from .region_finder import expand_labels
 from .roi import one_roi, calc_safe_roi
 from .skeleton import skeletonize
-from .slice import slice, slice_segment
+from .slice import slice, slice_segment, calc_max_slice_idx
 from .connected_components import connected_components
